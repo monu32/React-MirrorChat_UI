f973cc10a00b23ba6d9ce0ed8f325792d8718aef
Monu.Vishwakarma <monuvishwakarmabpl@gmail.com>
2022-03-01 14:13:13 +0530

    Updates

---
+++ precache-manifest.5f8dcc6da55e6c5276bd960b845cf6cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6897333841b6bf297aa7f2c3d78b8579",
+    "url": "/React-MirrorChat_UI/index.html"
+  },
+  {
+    "revision": "bf2c96ebdd2b5c297d9a",
+    "url": "/React-MirrorChat_UI/static/css/main.ce9b1dde.chunk.css"
+  },
+  {
+    "revision": "b2af3736fa68d06d4864",
+    "url": "/React-MirrorChat_UI/static/js/2.b1c3158a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-MirrorChat_UI/static/js/2.b1c3158a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2c96ebdd2b5c297d9a",
+    "url": "/React-MirrorChat_UI/static/js/main.74a490c7.chunk.js"
+  },
+  {
+    "revision": "f920b25beebd90290e16",
+    "url": "/React-MirrorChat_UI/static/js/runtime-main.cc53f42d.js"
+  },
+  {
+    "revision": "f6d01093002a01575c953968d79b0430",
+    "url": "/React-MirrorChat_UI/static/media/chatImage.f6d01093.png"
+  }
+]);
